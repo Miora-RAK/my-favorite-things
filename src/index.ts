2b3cb9cf9e5162be9674801ec40aed41eda3241f
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-10 03:13:29 +0100

    add image key

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,33 @@ import express from "express";
 import nunjucks from "nunjucks";
 
 type Book = {
+  image: string;
   name: string;
+  slug: string;
   author: string;
   year: string;
 };
 type Books = Book[];
 
 const myBooks: Books = [
-  { name: "Thinking fast and Slow", author: "Daniel Kahneman", year: "2012" },
-  { name: "Indistractable: How to Control Your Attention and Choose Your Life", author: "Nir Eyal", year: "2020" },
   {
+    image: "../images/thinking-fast-and-slow.jpeg",
+    name: "Thinking fast and Slow",
+    slug: "thinking-fast-and-Slow",
+    author: "Daniel Kahneman",
+    year: "2012",
+  },
+  {
+    image: "../images/indistractable.jpeg",
+    name: "Indistractable: How to Control Your Attention and Choose Your Life",
+    slug: "indistractable",
+    author: "Nir Eyal",
+    year: "2020",
+  },
+  {
+    image: "../images/the-lazy-genius.jpeg",
     name: "The Lazy Genius Way: Embrace What Matters, Ditch What Doesn't, and Get Stuff Done",
+    slug: "the-lazy-genius-way",
     author: "Kendra Adachi",
     year: "2020",
   },
@@ -36,7 +52,7 @@ app.get("/", (request, response) => {
 app.get("/:bookName", (request, response) => {
   const routeParameters = request.params;
   const bookSelected = routeParameters.bookName;
-  const findBook = myBooks.find((book) => book.name === bookSelected);
+  const findBook = myBooks.find((book) => book.slug === bookSelected);
 
   if (findBook) {
     response.render("details", { parameterValue: bookSelected, myBooks: myBooks });
